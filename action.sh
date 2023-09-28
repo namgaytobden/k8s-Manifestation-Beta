2a1db8a55b602e91ddd8eb18992d56a55471d97d
Namgay Tobden <121597739+namgaytobden@users.noreply.github.com>
2023-09-28 15:29:29 +0600

    update: typo

--- action.sh
+++ action.sh
@@ -1,3 +1,3 @@
 echo "$DOCKER_IMAGE"
 
-grep -r "image:" $MANIFEST_PATH | awk -F ':' '{print $1}' | xargs -I {} yq -i e '.spec.template.spec.containers[0].image = '"$DOCKER_IMAGE"'' {}
+grep -r "image:" $MANIFEST_PATH | awk -F ':' '{print $1}' | xargs -I {} yq -i e '.spec.template.spec.containers[0].image = '$DOCKER_IMAGE'' {}
