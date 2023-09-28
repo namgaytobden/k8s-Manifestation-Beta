dc86fc63dc03d69f519dabfa421a4726c87c4741
Namgay Tobden <121597739+namgaytobden@users.noreply.github.com>
2023-09-28 15:54:20 +0600

    Update: alternative for yd

--- action.sh
+++ action.sh
@@ -1,2 +1,3 @@
 echo "$DOCKER_IMAGE"
 grep -r "image:" $MANIFEST_PATH | awk -F ':' '{print $1}' | xargs -I {} yq -i e '.spec.template.spec.containers[0].image = "'${DOCKER_IMAGE}'"' {}
+#grep -r "image:" $MANIFEST_PATH | awk -F ':' '{print $1}' | xargs -I {}  {} sed -i 's|<IMAGE>|'${DOCKER_IMAGE}'|' {}
