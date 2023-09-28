5e9dd6a1dea4fa07be03d1fa1d7dc531fafe4d1c
Namgay Tobden <121597739+namgaytobden@users.noreply.github.com>
2023-09-28 15:12:26 +0600

    env manupulate

--- action.sh
+++ action.sh
@@ -1,1 +1,4 @@
-grep -r "image:" $MANIFEST_PATH | awk -F ':' '{print $1}' | xargs -I {} yq -i e '.spec.template.spec.containers[0].image = ['$DOCKER_IMAGE']' {}
+
+echo "$DOCKER_IMAGE=$Image" >> $GITHUB_ENV
+
+grep -r "image:" $MANIFEST_PATH | awk -F ':' '{print $1}' | xargs -I {} yq -i e '.spec.template.spec.containers[0].image = ['$Image']' {}
