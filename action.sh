3288048703571a3ee57aa5d8f9bedadc344ef944
Namgay Tobden <121597739+namgaytobden@users.noreply.github.com>
2023-09-28 15:39:03 +0600

    update: image env test

--- action.sh
+++ action.sh
@@ -1,3 +1,2 @@
 echo "$DOCKER_IMAGE"
-$image = $DOCKER_IMAGE
 grep -r "image:" $MANIFEST_PATH | awk -F ':' '{print $1}' | xargs -I {} yq -i e '.spec.template.spec.containers[0].image = "${DOCKER_IMAGE}" {}
