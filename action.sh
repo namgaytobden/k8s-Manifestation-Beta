0bf883af443bc82928938f3b8cb04292ab1dd25c
Namgay Tobden <121597739+namgaytobden@users.noreply.github.com>
2023-09-28 15:22:30 +0600

    update: env test 2

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 
-echo "$DOCKER_IMAGE=$Image" >> $GITHUB_ENV
-echo "${{ env.Image }}"
+echo "$DOCKER_IMAGE" >> $GITHUB_ENV
+echo "${{ env.DOCKER_IMAGE }}"
 
 grep -r "image:" $MANIFEST_PATH | awk -F ':' '{print $1}' | xargs -I {} yq -i e '.spec.template.spec.containers[0].image = w' {}
